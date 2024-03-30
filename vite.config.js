7c6879ddce5d1ef018d265b6267872ce78577870
[cj-2123] <cj2123acct@gmail.com>
2024-03-30 16:48:27 -0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/kh1-rando-checklist",
   plugins: [react()],
-})
+});
